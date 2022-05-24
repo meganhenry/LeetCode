b9ac73551ce4696ac173b869a5bd1ae5192616f1
Megan Henry <75574688+meganhenry@users.noreply.github.com>
2022-05-24 11:06:12 -0700

    Time: 90 ms (32.86%), Space: 41.7 MB (72.24%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let low = 0, high = n, mid = 0;
+        
+        while (high - low > 1) {
+         mid = Math.floor((high +  low)/2)
+            if (isBadVersion(mid)) {
+                high = mid;
+            } else {
+                low = mid;
+            }
+        }
+        return high;
+    };
+};
