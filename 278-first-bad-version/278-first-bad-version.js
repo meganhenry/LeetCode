05136088ed6980ba323305000906c78a0fa0f3f5
Megan Henry <75574688+meganhenry@users.noreply.github.com>
2022-05-26 14:50:36 -0700

    Revision
    Edited the high and low variable changes to include the +1 and -1 shift from the mid variable.

--- 278-first-bad-version/278-first-bad-version.js
+++ 278-first-bad-version/278-first-bad-version.js
@@ -23,9 +23,9 @@ var solution = function(isBadVersion) {
         while (high - low > 1) {
          mid = Math.floor((high +  low)/2)
             if (isBadVersion(mid)) {
-                high = mid;
+                high = mid - 1;
             } else {
-                low = mid;
+                low = mid + 1;
             }
         }
         return high;
