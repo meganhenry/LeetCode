29b4eddfd40baa400fce5d27f427246a4b64dea1
Megan Henry <75574688+meganhenry@users.noreply.github.com>
2022-05-26 15:18:13 -0700

    Time: 122 ms (68.23%), Space: 49 MB (24.48%) - LeetHub

---
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    for (var i = 0; i < nums.length; i++) {
+        nums[i] **= 2;
+    }
+    nums.sort(function(a,b){return a-b})
+    return nums;
+};
