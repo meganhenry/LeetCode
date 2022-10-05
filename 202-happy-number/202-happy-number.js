c6c5260474b50702f8ee7ecc8338fc924319d1dc
Megan Henry <75574688+meganhenry@users.noreply.github.com>
2022-10-04 22:24:55 -0700

    Time: 122 ms (24.85%), Space: 43.1 MB (72.32%) - LeetHub

---
+++ 202-happy-number/202-happy-number.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n, sum=0, n2 = 10) {
+    if(n === 1) return true;
+    if(n2 === 0) return false;
+    for(let c of n+'') sum += +c * +c;
+    return isHappy(sum, 0, n2 -1);
+};
